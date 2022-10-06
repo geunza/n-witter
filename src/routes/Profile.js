02f558e7bafbcff09f93271c675538ee12cc8eac
geunza <safa940812@gmail.com>
2022-10-07 01:32:45 +0900

    로그아웃, 로그아웃시 네비게이트

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { auth } from "fBase";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = ({ isLoggedIn }) => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    auth.signOut();
+    navigate("/");
+  };
+  useEffect(() => {
+    if (!isLoggedIn) navigate("/");
+  }, [isLoggedIn]);
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+    </>
+  );
+};
 export default Profile;

66556320c9bda7ef61563df941c11a6a02441324
geunza <65527146+geunza@users.noreply.github.com>
2022-10-07 18:30:22 +0900

    firstore 추가

--- src/fBase.js
+++ src/fBase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,3 +15,4 @@ initializeApp(firebaseConfig);
 
 export const firebaseInstance = firebaseConfig;
 export const auth = getAuth();
+export const dbService = getFirestore();

f085e3ff50bd542d8f71ee20473d13f6b1fcfaee
geunza <safa940812@gmail.com>
2022-10-11 01:25:00 +0900

    Nweet 수정,삭제 기능 추가.

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,13 +1,54 @@
-import React from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { dbService } from "../fBase";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+  const toggleEditing = () => {
+    setEditing((prev) => !prev);
+  };
+  const onChangeText = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
+  const onSubmitEdit = async (e) => {
+    e.preventDefault();
+    const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+    await updateDoc(NweetTextRef, { text: newNweet });
+    setEditing((prev) => !prev);
+  };
+  const onDeleteClick = async () => {
+    const ok = window.confirm("삭제하시겠습니까?");
+    if (ok) {
+      const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+      await deleteDoc(NweetTextRef);
+      // delete nweet
+    }
+  };
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
+      {editing ? (
+        <>
+          <form>
+            <input
+              value={newNweet}
+              placeholder="Edit your Nweet"
+              onChange={onChangeText}
+              required
+            />
+          </form>
+          <button onClick={onSubmitEdit}>Editing?</button>
+        </>
+      ) : (
+        <h4>{nweetObj.text}</h4>
+      )}
       {isOwner && (
         <>
-          <button>Delete Nweet</button>
-          <button>Edit Nweet</button>
+          <button onClick={onDeleteClick}>Delete Nweet</button>
+          <button onClick={toggleEditing}>Edit Nweet</button>
         </>
       )}
       <div>{isOwner}</div>
